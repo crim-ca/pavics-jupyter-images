85f3da35a0ae0c7a08cc562dc284627ebe75d83a
Timea Bagosi <timea.bagosi@crim.ca>
2020-12-08 16:25:13 -0500

    basic evaluation of queries, given a json with nlp to structured query examples. also add querying per individual catalog, or per a collection of catalogs

--- nlp/stac_wrapper/query_handler.py
+++ nlp/stac_wrapper/query_handler.py
@@ -1,5 +1,7 @@
 from intake import open_stac_catalog
 import satsearch
+import json
+import ipywidgets as widgets
 
 # STAC index public catalogs
 # https://stacindex.org/catalogs?access=true&type=null
@@ -11,10 +13,10 @@ stac_catalogs = {
     'earth_aws':  "https://earth-search.aws.element84.com/v0"
 }
 data_sources = {
-    'All': ['landsat8', 'google_engine', 'pangeo_cmip6', 'nasa_cmr', 'earth_aws'],
     'Climate': ['pangeo_cmip6'],
     'EO': ['landsat8', 'google_engine', 'nasa_cmr', 'earth_aws']
 }
+data_sources['All'] = data_sources['Climate'] + data_sources['EO']
 
 # set the default catalog
 catalog_URL = stac_catalogs['earth_aws']
@@ -22,8 +24,69 @@ catalog_URL = stac_catalogs['earth_aws']
 def inspect_catalog(catalog_URL):
     # open catalog
     catalog = open_stac_catalog(catalog_URL)
+    return catalog
 
-def query_nlp(query_text):
+def eval_query(query_text, catalogue, query_structured=None):
+    """
+    Method to evaluate how correctly the natural language query
+    was translated into a structured query.
+    Score is calculated based on the parameters:
+    collections, bbox, datetime, intersects and query
+    It returns precision, recall and f1 scores
+    """
+    recall_score = 0
+    precision_score = 0
+    f1_score = 0
+    if not query_structured:
+        query_structured = nlp2query(query_text)
+    with open('stac_wrapper/eval_queries.json', 'r') as f:
+        eval_base = json.load(f)
+    if query_text in eval_base[catalogue].keys():
+        print("Found query in evaluation set.")
+        gold_structured = eval_base[catalogue][query_text]
+        print("Gold parameters: ", gold_structured)
+        # parameter-wise comparison calculation of score
+        # recall
+        out_of = len(gold_structured.keys())
+        for param in gold_structured.keys():
+            inner_score = 0
+            if param in query_structured.keys():
+                gold_values = gold_structured[param]
+                query_values = query_structured[param]
+                if type(gold_values) == list and type(query_values) == list:
+                    nr_values = len(gold_values)
+                    for value in gold_values:
+                        # strict match values
+                        if value in query_values:
+                            inner_score += 1/nr_values
+                elif str(gold_values) == str(query_values):
+                    inner_score += 1
+            recall_score += inner_score / out_of
+        # precision
+        out_of = len(query_structured.keys())
+        for param in query_structured.keys():
+            inner_score = 0
+            if param in gold_structured.keys():
+                query_values = query_structured[param]
+                gold_values = gold_structured[param]
+                if type(gold_values) == list and type(query_values) == list:
+                    nr_values = len(query_values)
+                    for value in query_values:
+                        # strict match values
+                        if value in gold_values:
+                            inner_score += 1 / nr_values
+                elif str(gold_values) == str(query_values):
+                    inner_score += 1
+                precision_score += inner_score / out_of
+    else:
+        print("Query not found in evaluation set.")
+
+    if precision_score:
+        f1_score = (2 * precision_score * recall_score) / (precision_score + recall_score)
+    return {"precision": precision_score, "recall": recall_score, "f1": f1_score}
+
+
+def nlp2query(query_text):
     """
     Handle a NL query into a structured query
     returns parameters used for faceted search.
@@ -33,12 +96,14 @@ def query_nlp(query_text):
     # extract from the text query the structured query parameters
 
     # fill in parameters of structured query
-    params['bbox'] = [-110, 39.5, -105, 40.5]
-    params['datetime'] = '2018-02-12T00:00:00Z/2018-03-18T12:31:12Z'
-    params['query'] = ["eo:cloud_cover<10"]
-    params['collections'] = ['sentinel-s2-l2a']
+    if query_text == "Sentinel-2 over Ottawa from april to september 2018 with cloud cover lower than 10%":
+        params['collections'] = ['sentinel-s2-l2a']
+        params['bbox'] = [-110, 39.5, -105, 40.5]
+        params['datetime'] = '2018-04-01T00:00:00Z/2018-09-30T00:00:00Z'
+        params['query'] = ["eo:cloud_cover<10"]
     print("Created structured query with parameters: ", params)
     return params
+
 
 def handle_query(query_text, data_source='All'):
     """
@@ -47,15 +112,22 @@ def handle_query(query_text, data_source='All'):
     Returns result items or None.
     """
     print("Received query: ", query_text)
-    params = query_nlp(query_text)
-    for ds in data_sources[data_source]:
+    params = nlp2query(query_text)
+    # query a collection
+    if type(data_source) == list and data_source in data_sources:
+        for ds in data_sources[data_source]:
+            # TODO! remove this condition for full version.
+            #  This is demo query on earth_aws. Ignoring any other type of query for now.
+            if ds == 'earth_aws':
+                print("Querying data source: %s (%s)" % (ds, stac_catalogs[ds]))
+                search_query(params, stac_catalogs[ds])
+    # query a single catalog
+    elif data_source in data_sources['All']:
         # TODO! remove this condition for full version.
-        #  This is demo query on earth_aws. Ignore the rest for now.
-        if ds == 'earth_aws':
-            print("Querying data source: %s (%s)" % (ds, stac_catalogs[ds]))
-            search_query(params, stac_catalogs[ds])
-    return
-
+        #  This is demo query on earth_aws. Ignoring any other type of query for now.
+        if data_source == 'earth_aws':
+            print("Querying data source: %s (%s)" % (data_source, stac_catalogs[data_source]))
+            search_query(params, stac_catalogs[data_source])
 
 def search_query(params, catalog_URL):
     """
@@ -63,7 +135,7 @@ def search_query(params, catalog_URL):
     return result items or None.
     """
     results = satsearch.Search.search(url=catalog_URL,
-                                      bbox = params['bbox'],
+                                      bbox=params['bbox'],
                                       datetime=params['datetime'],
                                       query=params['query'],
                                       collections=params['collections'])
