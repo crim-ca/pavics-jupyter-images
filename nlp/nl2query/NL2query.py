3700ed29c27600535ce5f17de7e436cd8a7970f1
Timea Bagosi <bagositi@crim.ca>
2021-04-22 12:10:07 -0400

    modify to interface and single method

--- nlp/nl2query/NL2query.py
+++ nlp/nl2query/NL2query.py
@@ -1,39 +1,11 @@
 
-def nlq_ner(nlq):
-    """
-    Named entity recognition module over a
-    natural language query.
-    Returns a list of detected NEs.
-    """
-    return []
+class NL2Query:
 
-def nlq_ter(nlq):
-    """
-    Temporal expression recognition module over a
-    natural language query.
-    Returns a list of detected TEs.
-    """
-    return []
-
-def nlq_var_val(nlq):
-    """
-    Variable and value recognition module over a
-    natural language query.
-    Returns a list of detected variable names and their values.
-    """
-    return []
-
-def nl2query(nlq):
-    """
-    Takes a natural language query and
-    processes it with different NLP modules:
-    - named entity recognition (NER)
-    - temporal expression recognition (TER)
-    - variable name and value detection (Var&Val)
-    Returns the equivalent structured query in a dict format.
-    """
-    struct_q = {}
-    struct_q['ner'] = nlq_ner(nlq)
-    struct_q['ter'] = nlq_ter(nlq)
-    struct_q['var_val'] = nlq_var_val(nlq)
-    return struct_q
+    def transform_nl2query(self, nlq: str, config: str = None) -> dict:
+        """
+        Takes a natural language query string and
+        an optional config file with parameters.
+        Transform the NL query into a structured query.
+        Returns the equivalent structured query in a dict format.
+        """
+        pass
