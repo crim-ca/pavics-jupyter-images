c098ca8bad25c0525d2c37c28ef42feb9c2b8721
Timea Bagosi <bagositi@crim.ca>
2021-04-23 13:46:58 -0400

    update interface, define typeddicts for resulting annotations formats, add mockup implementation of interface

---
+++ nlp/nl2query/MyNL2query.py
@@ -0,0 +1,66 @@
+import NL2query
+from NL2query import *
+
+class MyEngine:
+    """ mockup class of an annotator engine """
+    def __init__(self, config):
+        # read config file and set necessary parameters
+        return
+
+    def get_annotations(self, nlq: str):
+        # call the engine to annotate the nl query
+        # and return a list of engine-specific annotations
+        return [["geoname","some identified text"], ["named entity", "some other text"]]
+
+
+class MyNL2query(NL2query):
+    """ mockup class to try out NL2query interface"""
+    engine = None
+
+    def __init__(self, config: str = None):
+        # start my NL2query engine
+        self.engine = MyEngine(config)
+
+    def create_property_annotation(self, annotation) -> PropertyAnnotation:
+        # take annotation given by the engine
+        # and create appropriate typeddict annotation
+        # filling in each slot as required
+        return PropertyAnnotation(text=annotation[1], type="property", position=[],
+                                  name="", value="", value_type="", operation="")
+
+    def create_location_annotation(self, annotation) -> LocationAnnotation:
+        return LocationAnnotation(text=annotation[1], type="location", position=[],
+                                  matchingType="", name="", value={})
+
+    def create_temporal_annotation(self, annotation) -> TemporalAnnotation:
+        return TemporalAnnotation(text=annotation[1], type="tempex", position=[],
+                                  tempex_type="", target="", value={})
+
+    def create_target_annotation(self, annotation) -> TargetAnnotation:
+        return TargetAnnotation(text=annotation[1], type="target", position=[],
+                                target="", value={})
+
+    def transform_nl2query(self, nlq: str) -> QueryAnnotationsDict:
+        # get annotations from my engine
+        engine_results = self.engine.get_annotations(nlq)
+        # collect annotations in a list of typed dicts
+        annot_dicts = []
+        for result in engine_results:
+            # check the type and create appropriate annotatation type
+            if result[0] == "named entity":
+                annot_dicts.append(self.create_property_annotation(result))
+            elif result[0] == "geoname":
+                annot_dicts.append(self.create_location_annotation(result))
+            # elif etc...
+        # return a query annotations typed dict as required
+        return QueryAnnotationsDict(query=nlq, annotations=annot_dicts)
+
+
+if __name__ == "__main__":
+    query = "Sentinel-2 over Ottawa from april to september 2020 with cloud cover lower than 10%"
+    config_file = "config.ini"
+    # call my nl2query class
+    my_instance = MyNL2query(config_file)
+    # get the structured query from the nl query
+    structq = my_instance.transform_nl2query(query)
+    print("Structured query: ", structq)
--- nlp/nl2query/NL2query.py
+++ nlp/nl2query/NL2query.py
@@ -1,11 +1,93 @@
+from typing import TypedDict, List, Any
+
+class Annotation(TypedDict):
+    """ typed dict definition of one annotation.
+    must include these fields """
+    text: str
+    position: List[int, int]
+    type: str
+
+class QueryAnnotationsDict(TypedDict):
+    """ typed dict definition of all annotations for a query"""
+    query: str
+    annotations: List[Annotation]
+
+class PropertyAnnotation(Annotation):
+    """ typed dict definition for a property annotation
+    must include Annotation superclass fields
+    and additional ones defined here """
+    name: str
+    value: Any
+    value_type: str
+    operation: str
+
+class LocationAnnotation(Annotation):
+    """ typed dict definition for a location annotation
+        must include Annotation superclass fields
+        and additional ones defined here """
+    matchingType: str
+    name: str
+    value: Any
+
+class TemporalAnnotation(Annotation):
+    """ typed dict definition for a temporal annotation
+        must include Annotation superclass fields
+        and additional ones defined here """
+    tempex_type: str
+    target: str
+    value: Any
+
+class TargetAnnotation(Annotation):
+    """ typed dict definition for a target annotation
+        must include Annotation superclass fields
+        and additional ones defined here """
+    name: List[str]
+
 
 class NL2Query:
+    """ class defining the minimal interface
+    that any Nl2query module should implement
+    """
+    # one config file per nl2query implementation
+    config = None
 
-    def transform_nl2query(self, nlq: str, config: str = None) -> dict:
+    def init(self, config: str = None):
+        self.config = config
+
+    def transform_nl2query(self, nlq: str) -> QueryAnnotationsDict:
         """
         Takes a natural language query string and
-        an optional config file with parameters.
-        Transform the NL query into a structured query.
-        Returns the equivalent structured query in a dict format.
+        transforms it into a structured query
+        by calling the nl2query engine.
+        Returns the equivalent structured query in a
+        predefined query annotations dict format.
+        """
+        pass
+
+    def create_property_annotation(self, annotation) -> PropertyAnnotation:
+        """
+        Takes an annotation output by the nl2query engine and
+        transforms it into a predefined typed dict for property annotation.
+        """
+        pass
+
+    def create_location_annotation(self, annotation) -> LocationAnnotation:
+        """
+        Takes an annotation output by the nl2query engine and
+        transforms it into a predefined typed dict for location annotation.
+        """
+        pass
+
+    def create_temporal_annotation(self, annotation) -> TemporalAnnotation:
+        """
+        Takes an annotation output by the nl2query engine and
+        transforms it into a predefined typed dict for temporal annotation.
+        """
+        pass
+
+    def create_target_annotation(self, annotation) -> TargetAnnotation:
+        """
+        Takes an annotation output by the nl2query engine and
+        transforms it into a predefined typed dict for target annotation.
         """
         pass
