951489fe5976bee1cb3072a84794e6b534672b74
Timea Bagosi <bagositi@crim.ca>
2021-04-27 09:04:52 -0400

    add spacy v3 ner

---
+++ nlp/nl2query/NER.py
@@ -0,0 +1,85 @@
+from NL2query import *
+import spacy
+import re
+
+class NER(NL2Query):
+    """ mockup class to try out NL2query interface"""
+    spacy_engine = None
+
+    def __init__(self, config: str = None):
+        NL2Query.__init__(self, config)
+        # start my NL2query engine
+        self.spacy_engine = spacy.load("en_core_web_trf")
+#        self.spacy_engine = Language.from_config(config)
+
+    def create_property_annotation(self, annotation) -> PropertyAnnotation:
+        # take annotation given by the engine
+        # and create appropriate typeddict annotation
+        # filling in each slot as required
+
+        # default
+        val = annotation.text
+        val_type = "string"
+        operation = "eq"
+
+        # extract numerical value
+        if annotation.label_ in ["QUANTITY", "CARDINAL", "PERCENT"]:
+            val = re.search(r"[+-]?\d+", annotation.text).group(0)
+            if val:
+                val = int(val)
+            else:
+                val = re.search(r"[+-]?\d+(?:\.\d+)?", annotation.text).group(0)
+                val = float(val)
+            val_type = "integer"
+            # extract operation ex: "less than" -> "lt"
+            operation = ""
+        if annotation.label_ == "PERCENT":
+            val_type = "percentage"
+        return PropertyAnnotation(text=annotation.text, type="property", position=[annotation.start_char, annotation.end_char],
+                                  name="", value=val, value_type=val_type, operation=operation)
+
+    def create_location_annotation(self, annotation) -> LocationAnnotation:
+        # get gejson of location
+        geojson = {}
+        return LocationAnnotation(text=annotation.text, type="location", position=[annotation.start_char, annotation.end_char],
+                                  matchingType="", name=annotation.text, value=geojson)
+
+    def create_temporal_annotation(self, annotation) -> TemporalAnnotation:
+        # get standard dateformat from text
+        datetime = {}
+        return TemporalAnnotation(text=annotation.text, type="tempex", position=[annotation.start_char, annotation.end_char],
+                                  tempex_type="", target="", value=datetime)
+
+    def create_target_annotation(self, annotation) -> TargetAnnotation:
+        return TargetAnnotation(text=annotation.text, type="target", position=[annotation.start_char, annotation.end_char],
+                                name=[""])
+
+    def transform_nl2query(self, nlq: str) -> QueryAnnotationsDict:
+        # get annotations from my engine
+        doc = self.spacy_engine(nlq)
+        # collect annotations in a list of typed dicts
+        annot_dicts = []
+        for ent in doc.ents:
+            print(ent.text, ent.start_char, ent.end_char, ent.label_)
+            # check the type and create appropriate annotatation type
+            if ent.label_ in ["PERCENT", "QUANTITY", "ORDINAL", "CARDINAL",
+                              "PERSON", "NORP", "ORGANIZATION", "FACILITY"]:
+                annot_dicts.append(self.create_property_annotation(ent))
+            elif ent.label_ in ["GPE", "LOCATION"]:
+                annot_dicts.append(self.create_location_annotation(ent))
+            elif ent.label_ in ["DATE", "TIME"]:
+                annot_dicts.append(self.create_temporal_annotation(ent))
+            elif ent.label_ in []:
+                annot_dicts.append(self.create_target_annotation(ent))
+        # return a query annotations typed dict as required
+        return QueryAnnotationsDict(query=nlq, annotations=annot_dicts)
+
+
+if __name__ == "__main__":
+    query = "Sentinel-2 over Ottawa from april to september 2020 with cloud cover lower than 10%"
+    config_file = "spacy_config.cfg"
+    # call my nl2query class
+    my_instance = NER(config_file)
+    # get the structured query from the nl query
+    structq = my_instance.transform_nl2query(query)
+    print("Structured query: ", structq)
