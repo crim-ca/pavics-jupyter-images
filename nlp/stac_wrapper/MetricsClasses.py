82e52463aa43337975cba3d874986b27dad29c88
Timea Bagosi <bagositi@crim.ca>
2021-05-20 15:56:52 -0400

    add static method to DataMetrics class, and refactor creation

--- nlp/stac_wrapper/MetricsClasses.py
+++ nlp/stac_wrapper/MetricsClasses.py
@@ -10,16 +10,9 @@ class DataMetrics:
         self.total_annotation_location = total_loc
         self.total_annotation_tempex = total_temp
         self.total_annotation_target = total_targ
-        self.total_annotation_per_type = [self.total_annotation_property,
-                                          self.total_annotation_location,
-                                          self.total_annotation_tempex,
-                                          self.total_annotation_target]
         self.annotation_per_query_avg = annot_avg
         self.annotation_per_query_min = annot_min
         self.annotation_per_query_max = annot_max
-        self.annotation_per_query = [self.annotation_per_query_avg,
-                                     self.annotation_per_query_min,
-                                     self.annotation_per_query_max]
 
 
     def to_dict(self):
@@ -36,3 +29,18 @@ class DataMetrics:
                 }
 
 
+    @staticmethod
+    def create_data_metrics(total_ann: int = 0, total_prop: int = 0, total_loc: int = 0,
+                 total_temp: int= 0, total_targ: int = 0,
+                 annot_avg: float = 0.0, annot_min: int = 0, annot_max: int = 0):
+        return {"total_annotation": total_ann,
+                "total_annotation_per_type": {
+                    "property": total_prop,
+                    "location": total_loc,
+                    "tempex": total_temp,
+                    "target": total_targ
+                },
+                "annotation_per_query": {"avg": annot_avg,
+                                         "min": annot_min,
+                                         "max": annot_max}
+                }
--- nlp/stac_wrapper/query_eval.py
+++ nlp/stac_wrapper/query_eval.py
@@ -70,23 +70,23 @@ def eval_data(gold: Dict, test: Dict) -> Dict:
     # annotation count per type
     gold_types = [ann['type'] for ann in gold_annotations]
     # create data metrics class instance
-    gold_data = DataMetrics(total_ann=len(gold_annotations),
-                            total_prop=gold_types.count('property'),
-                            total_loc=gold_types.count('location'),
-                            total_temp=gold_types.count('tempex'),
-                            total_targ=gold_types.count('target'))
+    gold_data = DataMetrics.create_data_metrics(total_ann=len(gold_annotations),
+                                                total_prop=gold_types.count('property'),
+                                                total_loc=gold_types.count('location'),
+                                                total_temp=gold_types.count('tempex'),
+                                                total_targ=gold_types.count('target'))
     # we will count averages and global values outside this function
     # same structure for test
     test_annotations = test['annotations']
     test_types = [ann['type'] for ann in test_annotations]
-    test_data = DataMetrics(total_ann=len(test_annotations),
-                            total_prop=test_types.count('property'),
-                            total_loc=test_types.count('location'),
-                            total_temp=test_types.count('tempex'),
-                            total_targ=test_types.count('target'))
+    test_data = DataMetrics.create_data_metrics(total_ann=len(test_annotations),
+                                                total_prop=test_types.count('property'),
+                                                total_loc=test_types.count('location'),
+                                                total_temp=test_types.count('tempex'),
+                                                total_targ=test_types.count('target'))
     # create measures dictionary
-    data_measures = {'gold_data': gold_data.to_dict(),
-                     'test_data': test_data.to_dict()}
+    data_measures = {'gold_data': gold_data,
+                     'test_data': test_data}
 
     return data_measures
 
@@ -513,8 +513,8 @@ def calc_global_data_scores(data_dicts: List[Dict]) -> Dict:
     # a list of data dicts
     # a data dict is one 'data_measurements' dict
     data_scores = {
-        "gold_data": DataMetrics().to_dict(),
-        "test_data": DataMetrics().to_dict()
+        "gold_data": DataMetrics.create_data_metrics(),
+        "test_data": DataMetrics.create_data_metrics()
     }
 
     for data_type in DATA_TYPES:
