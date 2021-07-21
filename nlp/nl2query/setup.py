6978e913e07604a9622450008232314b70c0f179
Azur Handan <Azur.Handan@crim.ca>
2021-07-21 19:44:42 +0000

    adding vocab as package

--- nlp/nl2query/setup.py
+++ nlp/nl2query/setup.py
@@ -1,5 +1,6 @@
 from distutils.core import setup
 setup(name='nl2query',
       version='0.2.0',
-      py_modules=['NER_flair','NER_spacy','NL2QueryInterface','TER_heideltime','Vars_values_textsearch','MyNL2query'],
+      packages=['.','vocab'],
+      package_dir = {'.':'.'}
       )
